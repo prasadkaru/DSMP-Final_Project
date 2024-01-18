ca3eea4bb726d0ae44c11993ac452b5efc14ec53
prasadkaru <133613185+prasadkaru@users.noreply.github.com>
2024-01-18 15:00:14 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',redirectTo:'/security',pathMatch:'full'},
+  { path: 'security', loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule) },
+  { path: 'starter', loadChildren: () => import('./modules/starter/starter.module').then(m => m.StarterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SecurityComponent} from "./modules/security/security.component";
+import {StarterComponent} from "./modules/starter/starter.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
